7e27e81f759aebf3a6a1085bdb44b07617d8d68b
SHEIK REZWAN ALI <rezwanalishaik@gmail.com>
2024-05-15 19:56:14 +0530

    Update comments.py

---
+++ Basics/comments.py
@@ -0,0 +1,5 @@
+# This is Example of single line comments
+"""This is example of mutli line comments with triple double quotaions in python"""
+'''This is example of mutli line comments with triple double quotaions in python'''
+
+
